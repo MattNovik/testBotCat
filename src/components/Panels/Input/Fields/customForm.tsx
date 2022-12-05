ecc8cf329876fcaa4e266b6af640e3fddb3d8084
lockdur <matvej.novik@gmail.com>
2022-12-05 18:11:39 +0300

    fix

--- src/components/Panels/Input/Fields/customForm.tsx
+++ src/components/Panels/Input/Fields/customForm.tsx
@@ -10,65 +10,60 @@ interface IPropsCF {
     onSetValue?(name: string, value: string): void;
 }
 
-const CustomForm1: FC<IPropsCF> = memo(({
-    name,
-    placeholder,
-    required,
-    onSetValue
-}) => {
+const CustomForm1: FC<IPropsCF> = memo(({ name, placeholder, required, onSetValue }) => {
     const [view, setView] = useState(0);
     const [value, setValue] = useState('');
     const [error] = useState('');
 
     const handleSetName = (val: string) => {
-        if(!!val && onSetValue) {
+        if (!required && !val) val = 'Нет';
+        if (!!val && onSetValue) {
             onSetValue(name, val);
             setValue('');
         }
-    }
+    };
 
     const handleChange = (_name: string, value: string) => {
         setValue(value);
-    }
+    };
 
     return (
         <Fragment>
-            {
-                view === 0 ? (
-                    <div className='botcat-customform-name'>
-                        <div className='botcat-customform-name__col'>
-                            <Button kind='primary' onClick={() => setView(1)}>введите имя</Button>
-                        </div>
-                        <div className='botcat-customform-name__col'>
-                            <Button kind='light' onClick={() => handleSetName('anonymous')}>анонимно</Button>
-                        </div>
+            {view === 0 ? (
+                <div className='botcat-customform-name'>
+                    <div className='botcat-customform-name__col'>
+                        <Button kind='primary' onClick={() => setView(1)}>
+                            введите имя
+                        </Button>
                     </div>
-                ) : (
-                    <form className='botcat-customform-name'>
-                        <div className='botcat-customform-name__col botcat-customform-name__col--auto'>
-                            <TextField
-                                name={name}
-                                value={value}
-                                placeholder={placeholder}
-                                onChange={handleChange}
-                            />
-                        </div>
-                        <div className='botcat-customform-name__col botcat-customform-name__col--none'>
-                            <Button
-                                onlyIcons
-                                onClick={() => handleSetName(value)}
-                                disabled={required ? (!!error || !value) : false}
-                                type='submit'
-                            >
-                                <Icons.Send
-                                    size={30}
-                                    fill='#FFF'
-                                />
-                            </Button>
-                        </div>
-                    </form>
-                )
-            }
+                    <div className='botcat-customform-name__col'>
+                        <Button kind='light' onClick={() => handleSetName('anonymous')}>
+                            анонимно
+                        </Button>
+                    </div>
+                </div>
+            ) : (
+                <form className='botcat-customform-name'>
+                    <div className='botcat-customform-name__col botcat-customform-name__col--auto'>
+                        <TextField
+                            name={name}
+                            value={value}
+                            placeholder={placeholder}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className='botcat-customform-name__col botcat-customform-name__col--none'>
+                        <Button
+                            onlyIcons
+                            onClick={() => handleSetName(value)}
+                            disabled={required ? !!error || !value : false}
+                            type='submit'
+                        >
+                            <Icons.Send size={30} fill='#FFF' />
+                        </Button>
+                    </div>
+                </form>
+            )}
         </Fragment>
     );
 });
